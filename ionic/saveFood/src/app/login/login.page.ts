633ce8b19b824e181a19db8d41443936a2f6beaf
Kien Ngo <kenseverus@gmail.com>
2019-09-17 15:39:22 -0400

    [IONIC]: Completed Login functionalities

--- ionic/saveFood/src/app/login/login.page.ts
+++ ionic/saveFood/src/app/login/login.page.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { ToastController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
+export class User {
+  email: string;
+  password: string;
+}
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -7,9 +14,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  public user:User = new User();
+  constructor(public fAuth: AngularFireAuth, public toastController: ToastController, public router: Router) { }
 
   ngOnInit() {
   }
 
+  async login() {
+    try {
+      const signin = await this.fAuth.auth.signInWithEmailAndPassword(this.user.email, this.user.password);
+      if (signin) {
+        console.info('Successfully registered');
+        this.router.navigate(['/home']);
+      }
+    }
+    catch (err) { 
+      console.error(err);
+      this.showSignInError(err.message) ;
+    }
+  }
+
+  async showSignInError(msg: string) {
+    const toast = await this.toastController.create({
+      message: msg,
+      duration: 2000
+    });
+    toast.present();
+  }
 }

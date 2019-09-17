3cea3fd781534f99006fe081b9453d131de7529a
Kien Ngo <kenseverus@gmail.com>
2019-09-17 11:42:32 -0400

    [IONIC] Init for local Storage

---
+++ ionic/saveFood/src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+
+import { Platform } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss']
+})
+export class AppComponent {
+  constructor(
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar,
+    private storage: Storage
+  ) {
+    this.initializeApp();
+    storage.get('showSlide').then((val) => {
+      if (val === undefined) {
+        console.info('App is running for the first time after installation => Show intro slide');
+        storage.set('showSlide', false);
+        // Show slide
+
+      }
+      else {
+        console.info('This is not the first time load, show slide would not be loaded');
+      }
+    })
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
+  }
+}

bbac1731598a44f443db62c38fe76b9e07889668
Kien Ngo <kenseverus@gmail.com>
2019-11-10 23:08:24 -0500

    Removed email from updatable field

--- ionic/saveFood/src/app/update-profile/update-profile.page.ts
+++ ionic/saveFood/src/app/update-profile/update-profile.page.ts
@@ -11,8 +11,7 @@ export class UpdateProfilePage implements OnInit {
   @Input() userID: string;
   newInfo: any = {
     displayName: '',
-    phoneNumber: '',
-    email: ''
+    phoneNumber: ''
   }
   constructor(public modalCtrl: ModalController, public userService: UserService) { }
 
